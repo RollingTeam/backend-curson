906d48a05f1ae2f93138c5c37d3075d6ba3c59e3
Belen Neme <belenneme1@gmail.com>
2021-01-01 23:19:26 -0300

    Fix en server file

--- server/server.js
+++ server/server.js
@@ -5,10 +5,9 @@ const mongoose = require("mongoose")
 const bodyParser= require("body-parser")
 const cors = require("cors");
 
-const cors = require('cors')
 const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./rutas/index'));
